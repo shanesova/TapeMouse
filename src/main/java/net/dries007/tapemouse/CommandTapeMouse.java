2d6d81ef43054f44a672e46800ae37c1984adcce
Shane Sova <ssova@yahoo.com>
2017-01-23 04:13:20 -0700

    Now there is a delay for key up and key down, also requires Java 8

--- src/main/java/net/dries007/tapemouse/CommandTapeMouse.java
+++ src/main/java/net/dries007/tapemouse/CommandTapeMouse.java
@@ -18,6 +18,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.lwjgl.input.Keyboard.KEY_NONE;
@@ -26,105 +27,101 @@ import static org.lwjgl.input.Keyboard.KEY_NONE;
  * @author Dries007
  */
 @SideOnly(Side.CLIENT)
-public class CommandTapeMouse extends CommandBase
-{
+public class CommandTapeMouse extends CommandBase {
     private static final List<KeyBinding> KEYBIND_ARRAY = ReflectionHelper.getPrivateValue(KeyBinding.class, null, "KEYBIND_ARRAY", "field_74516_a");
     private boolean prevPauseSetting = true; // defaults to true
 
     @Override
-    public String getCommandName()
-    {
+    public String getName() {
         return TapeMouse.MODID.toLowerCase();
     }
 
     @Override
-    public boolean checkPermission(MinecraftServer server, ICommandSender sender)
-    {
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
         return sender instanceof EntityPlayer;
     }
 
     @Override
-    public String getCommandUsage(ICommandSender sender)
-    {
-        return '/' + getCommandName() + " [off|keybinding name ...] [delay] => Use no arguments to get a list of keybindings.";
+    public String getUsage(ICommandSender sender) {
+        return '/' + getName() + " [off|keybinding name...] <key down time> <key up time>\n" +
+                "Use no arguments to get a list of keybindings.\n" +
+                "Note: if key down and key up times are both 0 then the key will be held down.\n" +
+                "Example: /" + getName() + " attack 20 20\n";
     }
 
     @Override
-    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
-    {
-        if (args.length == 0)
-        {
-            sender.addChatMessage(new TextComponentString("List of keybindings").setStyle(new Style().setColor(TextFormatting.AQUA)));
-            sender.addChatMessage(new TextComponentString("Key => NAME (category)").setStyle(new Style().setColor(TextFormatting.AQUA)));
-            for (KeyBinding keyBinding : KEYBIND_ARRAY)
-            {
-                if (keyBinding == null || keyBinding.getKeyCode() == KEY_NONE) continue;
-                String name = keyBinding.getKeyDescription();
-                if (name == null) continue;
-                name = name.replaceFirst("^key\\.", "");
-
-                String cat = keyBinding.getKeyCategory();
-                if (cat == null) continue;
-                cat = cat.replaceFirst("^key\\.", "");
-                cat = cat.replaceFirst("^categories\\.", "");
-
-                sender.addChatMessage(new TextComponentString(keyBinding.getDisplayName() + " => " + name + " (" + cat + ")"));
-            }
-            return;
-        }
-        if (args[0].equalsIgnoreCase("off"))
-        {
-            Minecraft.getMinecraft().gameSettings.pauseOnLostFocus = prevPauseSetting;
-            TapeMouse.keyBinding = null;
-            TapeMouse.i = 0;
-            sender.addChatMessage(new TextComponentString("TapeMouse off."));
-        }
-        else
-        {
-            String askedName = args[0];
-            if (args.length > 1)
-            {
-                try
-                {
-                    TapeMouse.delay = parseInt(args[args.length - 1], 0);
-                    for (int i = 1; i < args.length - 1; i++) askedName += ' ' + args[i];
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        switch (args.length) {
+            case 0:
+                ListKeybinds(sender);
+                break;
+            case 1:
+                if (args[0].equalsIgnoreCase("off")) {
+                    Minecraft.getMinecraft().gameSettings.pauseOnLostFocus = prevPauseSetting;
+                    TapeMouse.keyBinding = null;
+                    TapeMouse.i = 0;
+                    sender.sendMessage(new TextComponentString("TapeMouse off."));
+                } else {
+                    sender.sendMessage(new TextComponentString(getUsage(sender)));
                 }
-                catch (NumberInvalidException e)
-                {
-                    // Assume last part was not a number
-                    for (int i = 1; i < args.length; i++) askedName += ' ' + args[i];
+                break;
+            case 2:
+                sender.sendMessage(new TextComponentString(getUsage(sender)));
+                break;
+            default:
+                final String askedName = String.join(" ", Arrays.copyOfRange(args, 0, args.length - 2));
+
+                try {
+                    TapeMouse.keyDownDelay = parseInt(args[args.length - 2], 0);
+                    TapeMouse.keyUpDelay = parseInt(args[args.length - 1], 0);
+                } catch (NumberInvalidException e) {
+                    sender.sendMessage(new TextComponentString(getUsage(sender)));
                 }
-                askedName = askedName.trim();
-            }
-            for (KeyBinding keyBinding : KEYBIND_ARRAY)
-            {
-                if (keyBinding == null) continue;
-                String name = keyBinding.getKeyDescription();
-                if (name == null) continue;
-                name = name.replaceFirst("^key\\.", "");
-                if (askedName.equalsIgnoreCase(name))
-                {
+
+                KeyBinding keyBinding = KEYBIND_ARRAY
+                        .stream()
+                        .filter(str -> str.getKeyDescription().replaceFirst("^key\\.", "").equalsIgnoreCase(askedName))
+                        .findFirst().orElse(null);
+
+
+                if (keyBinding != null) {
                     prevPauseSetting = Minecraft.getMinecraft().gameSettings.pauseOnLostFocus;
                     Minecraft.getMinecraft().gameSettings.pauseOnLostFocus = false;
                     TapeMouse.keyBinding = keyBinding;
-                    sender.addChatMessage(new TextComponentString("TapeMouse on '" + keyBinding.getDisplayName() + "' with delay " + TapeMouse.delay + " ticks."));
-                    return;
+                    sender.sendMessage(new TextComponentString("TapeMouse on '" + keyBinding.getDisplayName() + "' with key down " + TapeMouse.keyDownDelay + " and key up " + TapeMouse.keyUpDelay + " ticks."));
+                } else {
+                    throw new CommandException(askedName + " is unknown keybinding.");
                 }
-            }
 
-            throw new CommandException(askedName + " is unknown keybinding. If your keybind ends in a number, you *have* to specify the delay.");
+                break;
         }
+
+    }
+
+    private void ListKeybinds(ICommandSender sender) {
+        sender.sendMessage(new TextComponentString("List of keybindings").setStyle(new Style().setColor(TextFormatting.AQUA)));
+        sender.sendMessage(new TextComponentString("Key => Name (category)").setStyle(new Style().setColor(TextFormatting.AQUA)));
+        for (KeyBinding keyBinding : KEYBIND_ARRAY) {
+            if (keyBinding == null || keyBinding.getKeyCode() == KEY_NONE) continue;
+            String name = keyBinding.getKeyDescription();
+            if (name == null) continue;
+            name = name.replaceFirst("^key\\.", "");
+
+            String cat = keyBinding.getKeyCategory();
+            if (cat == null) continue;
+            cat = cat.replaceFirst("^key\\.categories\\.", "");
+
+            sender.sendMessage(new TextComponentString(keyBinding.getDisplayName() + " => " + name + " (" + cat + ")"));
+        }
+        return;
     }
 
     @Override
-    public List<String> getTabCompletionOptions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos)
-    {
-        if (args.length == 1)
-        {
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos) {
+        if (args.length == 1) {
             List<String> list = new ArrayList<>();
             list.add("off");
-            for (KeyBinding keyBinding : KEYBIND_ARRAY)
-            {
+            for (KeyBinding keyBinding : KEYBIND_ARRAY) {
                 if (keyBinding == null || keyBinding.getKeyCode() == KEY_NONE) continue;
                 String name = keyBinding.getKeyDescription();
                 if (name == null) continue;
@@ -133,6 +130,6 @@ public class CommandTapeMouse extends CommandBase
             }
             return getListOfStringsMatchingLastWord(args, list);
         }
-        return super.getTabCompletionOptions(server, sender, args, pos);
+        return super.getTabCompletions(server, sender, args, pos);
     }
 }
--- src/main/java/net/dries007/tapemouse/TapeMouse.java
+++ src/main/java/net/dries007/tapemouse/TapeMouse.java
@@ -11,8 +11,6 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Main mod file
@@ -21,45 +19,52 @@ import net.minecraftforge.fml.relauncher.SideOnly;
  */
 @SuppressWarnings({"NewExpressionSideOnly", "MethodCallSideOnly"}) // clientSideOnly does about the same thing...
 @Mod(modid = TapeMouse.MODID, name = TapeMouse.NAME, dependencies = "before:*", useMetadata = false, clientSideOnly = true)
-public class TapeMouse
-{
+public class TapeMouse {
     public static final String MODID = "tapemouse";
     public static final String NAME = "TapeMouse";
-    static int delay = 10;
+    static int keyUpDelay = 0;
+    static int keyDownDelay = 0;
     static KeyBinding keyBinding;
     static int i = 0;
 
     @Mod.EventHandler
-    public void init(FMLPreInitializationEvent event)
-    {
+    public void init(FMLPreInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
         if (event.getSide().isClient()) ClientCommandHandler.instance.registerCommand(new CommandTapeMouse());
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public void textRenderEvent(RenderGameOverlayEvent.Text event)
-    {
+    public void textRenderEvent(RenderGameOverlayEvent.Text event) {
         if (keyBinding == null) return;
-        if (Minecraft.getMinecraft().currentScreen instanceof GuiChat)
-        {
+        if (Minecraft.getMinecraft().currentScreen instanceof GuiChat) {
             event.getLeft().add(MODID + " paused. Chat GUI open.");
-        }
-        else
-        {
+        } else {
             event.getLeft().add(MODID + " active: " + keyBinding.getDisplayName() + " (" + keyBinding.getKeyDescription().replaceFirst("^key\\.", "") + ')');
-            event.getLeft().add("Delay: " + i + " / " + delay);
+            event.getLeft().add("Delay: " + i + " / " + keyUpDelay + " / " + keyDownDelay);
         }
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public void tickEvent(TickEvent.ClientTickEvent event)
-    {
+    public void tickEvent(TickEvent.ClientTickEvent event) {
         if (event.phase != TickEvent.Phase.START) return;
         if (Minecraft.getMinecraft().currentScreen instanceof GuiChat) return;
         if (keyBinding == null) return;
-        if (i++ < delay) return;
-        i = 0;
-        if (delay == 0) KeyBinding.setKeyBindState(keyBinding.getKeyCode(), true);
-        KeyBinding.onTick(keyBinding.getKeyCode());
+
+        if (keyDownDelay == 0 && keyUpDelay == 0) {
+            if (!keyBinding.isKeyDown()) {
+                KeyBinding.setKeyBindState(keyBinding.getKeyCode(), true);
+                KeyBinding.onTick(keyBinding.getKeyCode());
+            }
+        } else {
+            i++;
+            if (keyBinding.isKeyDown() && i >= keyDownDelay) {
+                KeyBinding.setKeyBindState(keyBinding.getKeyCode(), false);
+                i = 0;
+            } else if (!keyBinding.isKeyDown() && i >= keyUpDelay) {
+                KeyBinding.setKeyBindState(keyBinding.getKeyCode(), true);
+                KeyBinding.onTick(keyBinding.getKeyCode());
+                i = 0;
+            }
+        }
     }
 }
